822a855206d74ec75e8c0e24c6e0e187f21a1792
Jack Frigaard <jack.frigaard@gmail.com>
2019-02-12 22:07:55 -0500

    Add extra fields

--- collect.py
+++ collect.py
@@ -1,38 +1,58 @@
-import geopy.distance
 import requests
 import os
 import time
-import pytz
 import datetime as dt
 
 STATION_INFO_URL = "https://gbfs.citibikenyc.com/gbfs/en/station_information.json"
 STATION_STATUS_URL = "https://gbfs.citibikenyc.com/gbfs/en/station_status.json"
-#MY_COORDS =
-DIST_THRESH_KM = 0.3
+
+FIELDS = [
+    'station_id', 
+    'num_bikes_available',
+    'num_ebikes_available',
+    'num_bikes_disabled',
+    'num_docks_available',
+    'num_docks_disabled',
+    'is_installed',
+    'is_renting',
+    'last_reported',
+]
 
 OUTPUT_FILE = './output.csv'
 
-stations_info = requests.get(STATION_INFO_URL).json()['data']['stations']
+def run_collector():
+    if not os.path.exists(OUTPUT_FILE):
+        with open(OUTPUT_FILE, 'w') as f:
+            f.write(','.join(['timestamp'] + FIELDS) + '\n')
 
-# station_subset = {s['station_id'] for s in stations_info if 
-    # geopy.distance.geodesic((s['lat'], s['lon']), MY_COORDS) < DIST_THRESH_KM}
-station_subset = {s['station_id'] for s in stations_info}
+    prev_last_updated = 0
+    
+    while True:
+        try:
+            stations_status = requests.get(STATION_STATUS_URL).json()
 
-fields = ['station_id', 'num_bikes_available', 'num_docks_available', 'last_reported']
+            last_updated = int(stations_status['last_updated'])
 
-if not os.path.exists(OUTPUT_FILE):
-    with open(OUTPUT_FILE, 'w') as f:
-        f.write(','.join(['timestamp'] + fields) + '\n')
+            if last_updated == prev_last_updated:
+                continue
 
-while True:
-    wakeup = dt.datetime.now(pytz.utc)
+            print(dt.datetime.now(), dt.datetime.fromtimestamp(prev_last_updated), dt.datetime.fromtimestamp(last_updated))
+            with open(OUTPUT_FILE, 'a') as h:
+                for s in stations_status['data']['stations']:
+                    h.write(str(last_updated))
+                    h.write(',')
+                    for f in FIELDS:
+                        h.write(str(s[f]))
+                        if f != FIELDS[-1]:
+                            h.write(',')
+                    h.write('\n')
 
-    stations_status = requests.get(STATION_STATUS_URL).json()['data']['stations']
+            prev_last_updated = last_updated
 
-    with open(OUTPUT_FILE, 'a') as f:
-        for s in stations_status:
-            f.write('{},'.format(wakeup))
-            f.write(','.join(str(s[f]) for f in fields))
-            f.write('\n')
+        except Exception as e:
+            print(e)
 
-    time.sleep(15)
+        time.sleep(6)
+
+if __name__ == "__main__":
+    run_collector()
